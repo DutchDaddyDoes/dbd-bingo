c868e6092550a4cd71ff4ea043983a8227a8e346
DutchDaddyDoes <126161001+DutchDaddyDoes@users.noreply.github.com>
2023-06-25 22:58:11 +0200

    Update Card.js
    Remove duplicates from randomized card

--- js/Card.js
+++ js/Card.js
@@ -40,13 +40,29 @@ class Card {
 
   randomizeCard() {
     // For each slot on Bingo Card (1-25, except 13)
+    let numbers = Array.apply(null, {length: 32}).map(Number.call, Number);
+    shuffle(numbers);
+
+    function shuffle(numbers) {
+      let currentIndex = numbers.length, randomIndex;
+
+      while(currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--;
+
+        [numbers[currentIndex],numbers[randomIndex]] = [numbers[randomIndex],numbers[currentIndex]];
+      }
+
+      return numbers;
+    }
+
     for (let slot = 0; slot < 26; slot++) {
       this.activeSlot = slot
       this.activeGridPosition = this.slotData.slotToGrid[slot]
 
       if (slot !== 13) {  
         // Get random integer between 0 and 32
-        const randomKillerNumber = Math.floor(Math.random() * Math.floor(32))
+        const randomKillerNumber = numbers[this.activeSlot]
         
         // Get Killer from `randomKillerNumber`
         const killer = this.killers.killers[randomKillerNumber]
